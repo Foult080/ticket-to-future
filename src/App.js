08e886dd0293dadac073b505b06c4d2d6ae0bdc5
foult080 <foult080@gmail.com>
2020-10-02 22:09:56 +0700

    simplest project for children

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import { Navbar, Nav, Button, Image, Form, Modal } from "react-bootstrap";
+import main from "./img/main.svg";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+
+  const OnChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+      <section className="modal">
+        <Modal
+          show={show}
+          onHide={handleClose}
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Modal.Header closeButton>
+            <Modal.Title>Hello {name}!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're doing great job!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              React is Great!
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </section>
+
+      <section className="nav-bar">
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Button variant="outline-primary">Button</Button>
+        </Navbar>
+      </section>
+
+      <section className="container">
+        <Image src={main} fluid />
+        <Form style={styles.title}>
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="John"
+            value={name}
+            onChange={OnChange}
+          />
+          <Form.Text className="text-muted">
+            Enter your Name and hit button
+          </Form.Text>
+          <Button
+            style={styles.button}
+            variant="outline-primary"
+            onClick={handleShow}
+          >
+            SAY
+          </Button>
+        </Form>
+      </section>
+    </Fragment>
   );
-}
+};
+
+const styles = {
+  title: {
+    textAlign: "center",
+    margin: "4rem",
+    fontSize: "2rem",
+    width: "auto",
+  },
+  button: {
+    fontSize: "1.5rem",
+    width: "auto",
+  },
+};
 
 export default App;
